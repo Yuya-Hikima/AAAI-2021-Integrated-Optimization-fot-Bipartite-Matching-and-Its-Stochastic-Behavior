7240139255c6294fdfebacfa82ce2e873b562804
Yuya Hikima <102900055+Yuya-Hikima@users.noreply.github.com>
2024-08-30 16:49:55 +0900

    Update Experiments_test.sh

--- Rideshare_experiment/Experiments_test.sh
+++ Rideshare_experiment/Experiments_test.sh
@@ -1,3 +1,3 @@
 cd bin
-python3 experiment_PL.py Manhattan 6 30 s 12
-python3 experiment_Sigmoid.py Manhattan 6 30 s 12
+python3 experiment_PL.py Manhattan 6 30 s 5
+python3 experiment_Sigmoid.py Manhattan 6 30 s 5
